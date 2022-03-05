d9b337a7a5b1eb7b6b8d37a29e3ad5912dccbda0
Thomas Ripoll <t-ripoll@legallais.com>
2022-03-05 13:33:02 +0100

    test example 	new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   spec/calculatorSpec.js 	new file:   spec/support/jasmine-browser.json 	new file:   src/calculator.js

---
+++ src/calculator.js
@@ -0,0 +1,9 @@
+function add(a,b){
+    return a+b;
+}
+
+function createElementJs(){
+    const element = document.createElement('div');
+    element.className = "blabla"
+    document.body.appendChild(element)
+}
